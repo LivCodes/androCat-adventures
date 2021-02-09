1e49a1657e794735c691cdc403f6428326a3601a
dreayoung <youngmandrea@gmail.com>
2021-02-08 21:49:53 -0500

    sprite showing

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ let canvasCtx = canvas.getContext('2d')
 
 const robotSprite = {}
 robotSprite.player = new Image()
-robotSprite.player.src = "./robot-sprite.png";
-debugger
-console.log(robotSprite)
+robotSprite.player.src = "./robo-sprite.png";
+const moves = ['up', 'down', 'left', 'right'];
 
-let playerHeight = '96.666px'
-let playerWidth = '89.333px'
+let playerHeight = 118.333
+let playerWidth = 80
 let playerFrameX = 0
 let playerFrameY = 0
 let spriteX = 0
 let spriteY = 0
+let speed = 0
 
 function drawSprite(img, sourceX, sourceY, sourceW, sourceH, destinationX, destinationY, destinationH, destinationW){
     canvasCtx.drawImage(img, sourceX, sourceY, sourceW, sourceH, destinationX, destinationY, destinationH, destinationW)
@@ -31,21 +31,34 @@ function drawSprite(img, sourceX, sourceY, sourceW, sourceH, destinationX, desti
 
 function animation(){
     canvasCtx.clearRect(0,0, canvas.width, canvas.height)
-    debugger
+    // debugger
     drawSprite(robotSprite.player, playerWidth * playerFrameX, playerHeight * playerFrameY, playerWidth, 
         playerHeight, spriteX, spriteY, playerWidth, playerHeight)
-        if (playerFrameX < 6){
+        if (playerFrameX < 9){
             playerFrameX++
         }
         else {
-            playerFrameX = 0
+            playerFrameX = 3
+        }
+
+        if (spriteX < canvas.width + playerWidth) {
+            spriteX += speed
+        }
+        else {
+            spriteX = 0 - playerWidth;
         }
 }
 
-setInterval(animation, 1000);
+setInterval(animation, 1000/30);
 
 // robot sprite
 // 804 x 580
 // 9 columns   width = 804 % 9 89.333
 // 6 rows      height = 580 % 96.666
+
+// fox sprite
+// 800 x 355
+// 10 cols       width = 800 % 10
+// 3 rows      height= 111 % 3
+
 });
